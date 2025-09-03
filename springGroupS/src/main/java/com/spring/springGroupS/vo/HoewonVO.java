5ab19795fe59f7550e2eec81ba1293163df3b95d
jigyeong-501 <wirud116@gmail.com>
2025-09-03 17:40:08 +0900

    20250903 17:40

---
+++ springGroupS/src/main/java/com/spring/springGroupS/vo/HoewonVO.java
@@ -0,0 +1,20 @@
+package com.spring.springGroupS.vo;
+
+import lombok.Data;
+
+//@Getter
+//@Setter
+//@ToString
+@Data //생성자 쓰면 오류
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
+public class HoewonVO {
+	private String mid;
+	private String pwd;
+	private String name;
+	private String strGender;
+	private String gender;
+	private int age;
+	private String nickName;
+}
---
+++ springGroupS/src/main/java/com/spring/springGroupS/vo/SiteInfor2VO.java
@@ -0,0 +1,11 @@
+package com.spring.springGroupS.vo;
+
+import lombok.Data;
+
+@Data
+public class SiteInfor2VO {
+	private String driver;
+	private String url;
+	private String user;
+	private String password;
+}
